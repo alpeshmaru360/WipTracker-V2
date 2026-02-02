d9c01d2af1746e9b67bcf92a3ab2382dbf0b102b
alpesh360 <alpeshmaru@strats360.com>
2026-02-02 11:44:38 +0530

    A Test First Comment
    A Test First Comment Code

--- app/Http/Controllers/QualityManager/QualityActionController.php
+++ app/Http/Controllers/QualityManager/QualityActionController.php
@@ -18,6 +18,8 @@ use App\Services\DashboardService;
 class QualityActionController extends Controller
 {
 
+
+	//test
     //after click on place order from inbox screen this function is called
     public function qualityAction(){
         return view('quality_manager.quality_action_form');
