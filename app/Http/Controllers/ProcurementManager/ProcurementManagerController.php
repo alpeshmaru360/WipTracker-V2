f6d7214a7a938d5818efd35b686965f32490c722
alpesh360 <alpeshmaru@strats360.com>
2026-02-06 17:08:55 +0530

    A Remove 'Pending BOM Check'
    A Remove 'Pending BOM Check' From Procurement Inbox.

--- app/Http/Controllers/ProcurementManager/ProcurementManagerController.php
+++ app/Http/Controllers/ProcurementManager/ProcurementManagerController.php
@@ -1159,15 +1159,7 @@ class ProcurementManagerController extends Controller
         $place_po_hours = AdminHoursManagement::where('lable', 'StandardProcessTimes')
             ->where('key', 'check_the_bom_and_place_po')
             ->where('is_deleted', 0)
-            ->value('value') ?? 48; // Default to 48 hours if not found
-
-        $bom_check = ProductsOfProjects::with('projects')
-            ->orderBy('id', 'desc')
-            ->where('bom_check_procurement_manager', '1')
-            ->get()->map(function ($item) use ($bomHours) {
-                $item->deadline = $this->calculateDeadline($item->created_at, $bomHours);
-                return $item;
-            });
+            ->value('value') ?? 48; // Default to 48 hours if not found       
 
         $pending_po_orders = ProductsOfProjects::with('projects')
             ->where('bom_check_procurement_manager', '3')
@@ -1200,37 +1192,19 @@ class ProcurementManagerController extends Controller
         $pending_bom = ProductsOfProjects::with('projects')
             ->orderBy('id', 'desc')
             ->whereNull('bom_path')
-            ->get();
-
-        // $drawing_check = ProductsOfProjects::with('projects')
-        //     ->orderBy('id', 'desc')
-        //     ->where('drawing_req_estimation_manager', '2')
-        //     ->where('drawing_check_procurement_manager', '1')
-        //     ->get();
+            ->get();        
 
         $bom_checked = ProductsOfProjects::with('projects')
             ->orderBy('id', 'desc')
             ->where('bom_req_estimation_manager', '2')
             ->where('bom_check_procurement_manager', '2')
-            ->get();
-
-        // $drawing_checked = ProductsOfProjects::with('projects')
-        //     ->orderBy('id', 'desc')
-        //     ->where('drawing_req_estimation_manager', '2')
-        //     ->where('drawing_check_procurement_manager', '2')
-        //     ->get();
-
-        $RejectedPurchaseOrders = PurchaseOrder::where(function ($query) {
-            $query->where('is_production_manager_approved', 2)
-                ->orWhere('is_production_engineer_approved', 2);
-        })->get() ?? collect();
+            ->get(); 
 
         $minimumLowStock = StockMasterModule::whereColumn('available_qty', '<=', 'minimum_required_qty')
             ->orderBy('id', 'asc')
             ->get();
 
-        //return view('procurement_manager.inbox', compact('bom_check', 'pending_bom', 'page_title', 'bom_checked', 'drawing_check', 'drawing_checked', 'RejectedPurchaseOrders', 'pending_po_orders', 'minimumLowStock'));
-        return view('procurement_manager.inbox', compact('bom_check', 'pending_bom', 'page_title', 'bom_checked', 'RejectedPurchaseOrders', 'pending_po_orders', 'minimumLowStock'));
+        return view('procurement_manager.inbox', compact('pending_bom', 'page_title', 'bom_checked', 'pending_po_orders', 'minimumLowStock'));
     }
 
     private function calculateDeadline($createdAt, $hours){
